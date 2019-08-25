01c27907ea09ab31df349f2d29eb85916b7ee902
poornima S <poornii87@gmail.com>
2019-08-25 18:08:03 +0100

    first commit

---
+++ src/learnProgramming/MethodChaining.java
@@ -0,0 +1,22 @@
+package learnProgramming;
+
+public class MethodChaining {
+
+    public static void main(String[] args) {
+
+        String start = "  Java  ";
+        System.out.println(start);
+        String trimmed = start.trim();
+        System.out.println(trimmed);
+        String lowerCase = trimmed.toLowerCase();
+        System.out.println(lowerCase);
+        String result = lowerCase.replace('j','J');
+        System.out.println(result);
+
+        String anotherResult = " Java ".trim().toLowerCase().replace('j','J');
+        System.out.println(anotherResult);
+
+
+
+    }
+}
